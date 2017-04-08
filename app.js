2771ce821e93d1b75380948f7320b55b02fb515a
Alexander Nikolov <al.a.nikolov@abv.bg>
2017-04-08 19:00:22 +0300

    Added port to app.js

--- app.js
+++ app.js
@@ -11,6 +11,8 @@ var pp = require('./routes/pp');
 
 var app = express();
 
+app.set('port', (process.env.PORT || 5000));
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
@@ -28,14 +30,14 @@ app.use('/users', users);
 app.use('/pp', pp);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   var err = new Error('Not Found');
   err.status = 404;
   next(err);
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
@@ -43,6 +45,10 @@ app.use(function(err, req, res, next) {
   // render the error page
   res.status(err.status || 500);
   res.render('error');
+});
+
+app.listen(app.get('port'), function () {
+  console.log('Node app is running on port', app.get('port'));
 });
 
 module.exports = app;
