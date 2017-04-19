b628d818f67a6872fa56f00cc74dc6d5c936a2eb
mplyulev <mplulev@gmail.com>
2017-04-19 22:40:14 +0300

    added sound icons with functionality

--- public/javascripts/website/indexFront.js
+++ public/javascripts/website/indexFront.js
@@ -1,10 +1,22 @@
 $(function() { 
      $("#logo2").delay(300).animate({opacity: '1'},1000);
-
-     
      $("#logo2").animate({top: '30'},500);
-  
- 
- 
-
+     $("#audio")[0].play();
+     var x = 1;
+     $("#volumeDown").on("click",function (){ 
+        if ($("#audio").prop("volume")>=0.1) {
+            $("#audio").prop("volume", x);
+            x-=0.1;
+        }
+     }); 
+     $("#volumeUp").on("click",function (){ 
+        if ($("#audio").prop("volume")<=0.9) {
+            x+=0.1;
+            $("#audio").prop("volume", x); 
+        }
+     }); 
+     $("#volumeOff").on("click",function (){ 
+         x=0;
+      $("#audio").prop("volume",x) ;  
+     });     
 });
