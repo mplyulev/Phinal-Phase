8a60db101c8d425a0aa28f085c93ca15df5395ec
unknown <mplulev@gmail.com>
2017-04-25 09:46:06 +0300

    sort alphabeticaly button added. my statistics tab added

--- routes/dataRanklist.js
+++ routes/dataRanklist.js
@@ -3,9 +3,9 @@ var router = express.Router();
 users=db.get("users");
 router.get('/', function(req, res, next) {
     users.find()
-        .then(function (data2) {  
-          console.log(data2);
-          res.json(data2);
+        .then(function (data) {  
+          console.log(data);
+          res.json(data);
   }).catch(function(err) {
     res.json(500 , err);
   }); 
