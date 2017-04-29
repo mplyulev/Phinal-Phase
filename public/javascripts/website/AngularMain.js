0f63fde2c912cfb878f690a1b4c20156de172413
unknown <mplulev@gmail.com>
2017-04-29 09:33:52 +0300

    password hashing and salting with bcrypt. change avatar connected with URL input model(angular).

--- public/javascripts/website/AngularMain.js
+++ public/javascripts/website/AngularMain.js
@@ -11,7 +11,7 @@ var app  = angular.module("myApp",["ngRoute"]);
          templateUrl: "/HTML/myStatistics.htm",
      }) 
      .when("/contacts", {
-         templateUrl: "/HTML/contacts.htm",
+         templateUrl: "/HTML/contacts.html",
      }) 
      .when("pp", {
          templateUrl: "/HTML/phinalphase.html",
@@ -25,13 +25,25 @@ console.log("check controller");
 });
  });
 
- app.controller("ranklistController",  function($scope,$http)  {
+app.controller("ranklistController",  function($scope,$http)  {
 $http.get("dataRanklist").then(function (response)  {
 $scope.data = response.data;
 console.log("check controller");
 });
+var roundedOne = angular.element( document.querySelector( '#roundedOne' ) );
+var ranklistContainer = angular.element( document.querySelector( '#ranklistContainer' ) );
+var ranklistMenuButton = angular.element( document.querySelector( '#ranklistMenuButton' ) );
+$(roundedOne).on("click", function () {
+    $(ranklistContainer).delay(1000).fadeOut(1000);
+});
+ $(ranklistMenuButton).on("click", function () {
+    $(ranklistContainer).fadeIn(1000); 
+    });
  });
- app.controller("nameSortController",  function($scope,$http)  {
+
+
+
+app.controller("nameSortController",  function($scope,$http)  {
     $scope.filterString = '';
     $scope.sortByName = false;
     $scope.sortOrder = '';
@@ -48,6 +60,37 @@ console.log("check controller");
     }
  });
  
+app.controller('messageController', function($scope) {
+    var messageArea = angular.element( document.querySelector( '#messageMail' ) );
+    var subject = angular.element( document.querySelector( '#subject' ) );
+    var sendButton = angular.element( document.querySelector( '#messageSubmit' ) );
+    var successMessage = angular.element( document.querySelector( '#success' ) );
+    var noSuccessMessage = angular.element( document.querySelector( '#noSuccess' ) );
+    var noSuccessMessage2 = angular.element( document.querySelector( '#noSuccess2' ) );
+    var form = angular.element( document.querySelector( '#messageForm' ) );
+    $(sendButton).on('click', function(event) {
+        function preventDefault(e) {
+            e.preventDefault();
+        }
+    //   $(form).bind("submit", preventDefault);    
+        if (messageArea.val().length > 1 && subject.val().length>0) { 
+                // $(form).unbind("submit", preventDefault);
+                //      $(form).trigger("submit");
+            $(successMessage).fadeIn(1000).delay(2500).fadeOut(1000);
+            $(successMessage).css("display","block");   
+        }
+        // $(messageArea).val(""); 
+        if (messageArea.val().length<1)  {
+            $(noSuccessMessage2).toggle();
+            $(noSuccessMessage).show(1000).delay(2500).fadeOut(1000);
+        }
+        if (subject.val().length<1)  {
+            $(noSuccessMessage2).toggle();
+            $(noSuccessMessage2).show(1000).delay(2500).fadeOut(1000);
+        }
+    });  
+});
+
 app.controller('usernameSortController', function($scope,$http) {
     $http.get("dataRanklist").then(function (response)  {
 $scope.data = response.data;
--- routes/login.js
+++ routes/login.js
@@ -1,20 +1,30 @@
 var express = require('express');
 var router = express.Router();
+var bcrypt = require('bcrypt');
+
+
+// Load the password hash from DB
+// Let's assume it's stored in a variable called `hash`
+ 
 
 var users = db.get('users');
 router.post('/', function (req, res, next) {
-    var usernameLogin = req.body.usernameLogin;
+    var usernameLogin = req.body.usernameLogin.trim();
     var passwordLogin = req.body.key;
-    users.find({ username: usernameLogin ,password: passwordLogin })
+    
+
+    users.find({ username: usernameLogin  })
         .then(function (data) {
-            if (data.length > 0) {
+           
+            if (data.length > 0 && bcrypt.compareSync(passwordLogin, data[0].password)) {
                 var loggedInUser = data[0].username;
-                console.log(data);
+                console.log(bcrypt.compareSync(passwordLogin, data[0].password));
                 req.session.userId = data[0]._id;
                 res.redirect('/index');
             } 
             else {
                 res.render('login', { message: 'Wrong username or password.' });
+                 console.log(bcrypt.compareSync(passwordLogin, data[0].password));
               
             }
         });
